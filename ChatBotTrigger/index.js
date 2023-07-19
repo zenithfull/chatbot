a1d1ee40e4a7ad36effda505892389be1cc6df9d
y.takahashi <y.takahashi@necomata.co.jp>
2023-07-19 18:55:37 +0900

    Slack連携認証するためのソース

--- ChatBotTrigger/index.js
+++ ChatBotTrigger/index.js
@@ -1,161 +1,19 @@
-const { WebClient } = require('@slack/web-api');
-const {
-  ChatCompletionRequestMessageRoleEnum,
-  Configuration,
-  OpenAIApi,
-} = require('openai');
+var request = require('superagent');
 
-const openaiClient = new OpenAIApi(
-  new Configuration({
-    apiKey: process.env.OPENAI_API_KEY,
-    basePath: process.env.OPENAI_API_URL + process.env.OPENAI_API_MODEL,
-    baseOptions: {
-      headers: { 'api-key': process.env.OPENAI_API_KEY },
-      params: {
-        'api-version': '2023-03-15-preview',
-      },
-    },
-  })
-);
-const slackClient = new WebClient(process.env.SLACK_BOT_TOKEN);
-const GPT_BOT_USER_ID = process.env.GPT_BOT_USER_ID;
-const CHAT_GPT_SYSTEM_PROMPT = process.env.CHAT_GPT_SYSTEM_PROMPT;
-const GPT_THREAD_MAX_COUNT = process.env.GPT_THREAD_MAX_COUNT;
-
-/**
- * Slackへメッセージを投稿する
- * @param {string} channel 投稿先のチャンネル
- * @param {string} text 投稿するメッセージ
- * @param {string} threadTs 投稿先がスレッドの場合の設定
- * @param {object} context Azure Functions のcontext
- */
-const postMessage = async (channel, text, threadTs, context) => {
-  await slackClient.chat.postMessage({
-    channel: channel,
-    text: text,
-    thread_ts: threadTs,
-  });
-  context.log(text);
-};
-
-/**
- * ChatGPTからメッセージを受け取る
- * @param {string} messages 尋ねるメッセージ
- * @param {object} context Azure Functions のcontext
- * @returns content
- */
-const createCompletion = async (messages, context) => {
-  try {
-    const response = await openaiClient.createChatCompletion({
-      messages: messages,
-      max_tokens: 800,
-      temperature: 0.7,
-      frequency_penalty: 0,
-      presence_penalty: 0,
-      top_p: 0.95,
-    });
-    return response.data.choices[0].message.content;
-  } catch (err) {
-    context.log.error(err);
-    return err.response.statusText;
-  }
-};
-
-module.exports = async function (context, req) {
-  // Ignore retry requests
-  if (req.headers['x-slack-retry-num']) {
-    context.log('Ignoring Retry request: ' + req.headers['x-slack-retry-num']);
-    context.log(req.body);
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: 'No need to resend' }),
-    };
-  }
-
-  // Response slack challenge requests
-  const body = eval(req.body);
-  if (body.challenge) {
-    context.log('Challenge: ' + body.challenge);
+module.exports = function (context, req) {
+  var body_type = req.body.type;
+  if (body_type == 'url_verification') {
+    var body_challenge = req.body.challenge;
     context.res = {
-      body: body.challenge,
+      body: body_challenge,
     };
-    return;
+  } else if (body_type == 'event_callback') {
+    // あとで実装
+  } else {
+    context.res = {
+      status: 400,
+      body: 'Bad Request. Please check your request parameters.',
+    };
   }
-
-  context.log(req.body);
-  const event = body.event;
-  const threadTs = event?.thread_ts ?? event?.ts;
-  if (event?.type === 'app_mention') {
-    try {
-      const threadMessagesResponse = await slackClient.conversations.replies({
-        channel: event.channel,
-        ts: threadTs,
-      });
-      if (threadMessagesResponse.ok !== true) {
-        await postMessage(
-          event.channel,
-          '[Bot]メッセージの取得に失敗しました。',
-          threadTs,
-          context
-        );
-        return;
-      }
-      const botMessages = threadMessagesResponse.messages
-        .sort((a, b) => Number(a.ts) - Number(b.ts))
-        .filter(
-          (message) =>
-            message.text.includes(GPT_BOT_USER_ID) ||
-            message.user == GPT_BOT_USER_ID
-        )
-        .slice(GPT_THREAD_MAX_COUNT * -1)
-        .map((m) => {
-          const role = m.bot_id
-            ? ChatCompletionRequestMessageRoleEnum.Assistant
-            : ChatCompletionRequestMessageRoleEnum.User;
-          return { role: role, content: m.text.replace(/]+>/g, '') };
-        });
-      if (botMessages.length < 1) {
-        await postMessage(
-          event.channel,
-          '[Bot]質問メッセージが見つかりませんでした。@chatgptbot を付けて質問してみて下さい。',
-          threadTs,
-          context
-        );
-        return;
-      }
-      context.log(botMessages);
-      var postMessages = [
-        {
-          role: ChatCompletionRequestMessageRoleEnum.System,
-          content: CHAT_GPT_SYSTEM_PROMPT,
-        },
-        ...botMessages,
-      ];
-      const openaiResponse = await createCompletion(postMessages, context);
-      if (openaiResponse == null || openaiResponse == '') {
-        await postMessage(
-          event.channel,
-          '[Bot]ChatGPTから返信がありませんでした。この症状は、ChatGPTのサーバーの調子が悪い時に起こります。少し待って再度試してみて下さい。',
-          threadTs,
-          context
-        );
-        return { statusCode: 200 };
-      }
-      await postMessage(event.channel, openaiResponse, threadTs, context);
-      context.log('ChatGPTBot function post message successfully.');
-      return { statusCode: 200 };
-    } catch (error) {
-      context.log(
-        await postMessage(
-          event.channel,
-          `Error happened: ${error}`,
-          threadTs,
-          context
-        )
-      );
-    }
-  }
-  context.res = {
-    status: 200,
-  };
+  context.done();
 };
